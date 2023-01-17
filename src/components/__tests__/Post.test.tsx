9929784354c34e46bb977aa8a3b507c46092c61a
Marcos Mendes <marcosdev.me@gmail.com>
2023-01-17 16:08:47 -0300

    test: make post tests

--- src/components/__tests__/Post.test.tsx
+++ src/components/__tests__/Post.test.tsx
@@ -5,18 +5,18 @@ import { screen, render } from '@testing-library/react'
 import Post, { PostProps } from '../Post'
 
 describe('<Post />', () => {
-  it('should render correctly', () => {
-    const props: PostProps = {
-      id: 1,
-      canonical_url: 'https://jsonplaceholder.typicode.com/posts/1',
-      cover_image:
-        'https://upload.wikimedia.org/wikipedia/commons/thumb/9/99/Unofficial_JavaScript_logo_2.svg/1200px-Unofficial_JavaScript_logo_2.svg.png',
-      reading_time_minutes: 5,
-      tag_list: ['test', 'js'],
-      title: 'Blog post 1',
-      type_of: 'blog post'
-    }
+  const props: PostProps = {
+    id: 1,
+    canonical_url: 'https://jsonplaceholder.typicode.com/posts/1',
+    cover_image:
+      'https://upload.wikimedia.org/wikipedia/commons/thumb/9/99/Unofficial_JavaScript_logo_2.svg/1200px-Unofficial_JavaScript_logo_2.svg.png',
+    reading_time_minutes: 5,
+    tag_list: ['test', 'js'],
+    title: 'Blog post 1',
+    type_of: 'blog post'
+  }
 
+  it('should render correctly', () => {
     render(<Post {...props} />)
 
     expect(
@@ -33,5 +33,18 @@ describe('<Post />', () => {
 
     expect(screen.getByText(props.title)).toBeInTheDocument()
     expect(screen.getByText(/5 min read/i)).toBeInTheDocument()
+  })
+
+  it('should a default image when props is not passed', () => {
+    render(<Post {...props} cover_image={undefined} />)
+
+    const currentPath = window.location.href
+    const defaultImagePath = 'assets/defaultPostImage.jpg'
+
+    expect(
+      screen.getByRole('img', {
+        name: props.title
+      })
+    ).toHaveProperty('src', `${currentPath}${defaultImagePath}`)
   })
 })
