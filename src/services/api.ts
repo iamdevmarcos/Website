e6f21f54eeca21fc1ccc77b24cb4743ce7db3ce6
Marcos Mendes <marcosdev.me@gmail.com>
2023-01-17 10:53:38 -0300

    feat: add current page

--- src/services/api.ts
+++ src/services/api.ts
@@ -3,7 +3,7 @@ import axios from 'axios'
 const DEV_TO_API_URL = process.env.DEV_TO_API_URL as string
 
 export async function getPostsByUsername(username: string) {
-  const endpoint = `${DEV_TO_API_URL}/articles?username=${username}`
+  const endpoint = `${DEV_TO_API_URL}/articles?username=${username}&page=1`
   const { data } = await axios.get(endpoint)
   return data
 }
