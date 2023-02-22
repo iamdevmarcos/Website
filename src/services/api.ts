c94e834d78199e5cc7be824c694867d4f85a76f6
Marcos Mendes <marcosdev.me@gmail.com>
2023-02-22 07:37:53 -0300

    refactor: use API_URL from env

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,7 +1,9 @@
 import axios from 'axios'
 
+const API_URL = process.env.NEXT_PUBLIC_DEV_TO_API_URL as string
+
 export async function getPostsByUsername() {
-  const endpoint = `https://dev.to/api/articles?username=iamdevmarcos`
-  const { data } = await axios.get(endpoint)
+  const response = `${API_URL}/articles?username=iamdevmarcos`
+  const { data } = await axios.get(response)
   return data
 }
