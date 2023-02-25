85ed8e9e24d23bfbb58c6e5bd7ceb9ccafeaad49
Marcos Mendes <marcosdev.me@gmail.com>
2023-02-25 20:41:18 -0300

    test: use native js fetch instead useQuery

--- src/sections/BlogPosts.tsx
+++ src/sections/BlogPosts.tsx
@@ -1,3 +1,4 @@
+import { useEffect } from 'react'
 import { useQuery } from 'react-query'
 
 import { Flex } from '@chakra-ui/react'
@@ -5,7 +6,19 @@ import { Container, Heading, Loading, Post } from 'components'
 import type { PostProps } from 'components/Post'
 import { getPostsByUsername } from 'services/api'
 
+const API_URL = process.env.NEXT_PUBLIC_DEV_TO_API_URL as string
+
 const BlogPosts = () => {
+  useEffect(() => {
+    async function fetchData() {
+      const response = await fetch(`${API_URL}/articles?username=iamdevmarcos`)
+      const data = await response.json()
+      console.log(data)
+    }
+
+    fetchData()
+  })
+
   const {
     isLoading,
     isError,
