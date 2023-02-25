86b6d73b16202d9156e62c026016fe5f712d129a
Marcos Mendes <marcosdev.me@gmail.com>
2023-02-25 14:46:42 -0300

    chore: remove compiler of styled components

--- jest.config.js
+++ jest.config.js
@@ -14,10 +14,6 @@ module.exports = {
     '!src/services/**'
   ],
   setupFilesAfterEnv: ['<rootDir>/.jest/setup.ts'],
-  moduleNameMapper: {
-    '^styled-components':
-      '<rootDir>/node_modules/styled-components/dist/styled-components.browser.cjs.js'
-  },
   modulePaths: ['<rootDir>/src/'],
   transform: {
     '^.+\\.(js|jsx|ts|tsx)$': ['babel-jest', { presets: ['next/babel'] }]
--- next.config.js
+++ next.config.js
@@ -2,9 +2,5 @@
 const withTM = require('next-transpile-modules')(['react-icons'])
 
 module.exports = withTM({
-  swcMinify: true,
-  compiler: {
-    // Enables the styled-components SWC transform
-    styledComponents: true
-  }
+  swcMinify: true
 })
