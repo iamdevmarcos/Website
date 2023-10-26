87920b2796b8590f119b8b5a848e0e3057abfad1
Marcos Mendes <marcosdev.me@gmail.com>
2023-10-26 15:45:20 -0300

    Use getServerSideProps instead Axios and React Query

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,5 @@
 import { Layout } from 'components'
+import { type PostProps } from 'components/Post'
 import {
   BlogPosts,
   ContributedProjects,
@@ -6,13 +7,29 @@ import {
   PersonalProjects
 } from 'sections'
 
-export default function Home() {
+export type HomeProps = {
+  posts: PostProps[]
+}
+
+export default function Home({ posts }: HomeProps) {
   return (
     <Layout>
       <Introduction />
-      <BlogPosts />
+      <BlogPosts posts={posts} />
       <PersonalProjects />
       <ContributedProjects />
     </Layout>
   )
+}
+
+export async function getServerSideProps() {
+  const data = await fetch(
+    `${process.env.DEV_TO_API_URL}/articles?username=iamdevmarcos`
+  )
+
+  return {
+    props: {
+      posts: await data.json()
+    }
+  }
 }
--- src/sections/BlogPosts.tsx
+++ src/sections/BlogPosts.tsx
@@ -1,38 +1,24 @@
-import { useQuery } from 'react-query'
-
 import { Flex } from '@chakra-ui/react'
-import { Container, Loading, Post, Section } from 'components'
-import type { PostProps } from 'components/Post'
-import { getPostsByUsername } from 'services/api'
+import { Container, Post, Section } from 'components'
+import { HomeProps } from 'pages'
 
-const BlogPosts = () => {
-  const {
-    isLoading,
-    isError,
-    data: posts
-  } = useQuery('posts', getPostsByUsername)
-
-  if (isLoading) return <Loading />
-  if (isError) throw new Error('Ops! Something went wrong...')
-
-  return (
-    <Container>
-      <Flex
-        as="section"
-        aria-label="Blog Posts and Articles"
-        mt={{ base: '6rem', sm: '8rem' }}
-        flexDir="column"
-        justifyContent="flex-start"
-        gap={10}
-      >
-        <Section heading="Blog Posts and Articles">
-          {posts.map((item: PostProps) => (
-            <Post {...item} key={item.id} />
-          ))}
-        </Section>
-      </Flex>
-    </Container>
-  )
-}
+const BlogPosts = ({ posts }: Pick<HomeProps, 'posts'>) => (
+  <Container>
+    <Flex
+      as="section"
+      aria-label="Blog Posts and Articles"
+      mt={{ base: '6rem', sm: '8rem' }}
+      flexDir="column"
+      justifyContent="flex-start"
+      gap={10}
+    >
+      <Section heading="Blog Posts and Articles">
+        {posts.map((item) => (
+          <Post {...item} key={item.id} />
+        ))}
+      </Section>
+    </Flex>
+  </Container>
+)
 
 export default BlogPosts
