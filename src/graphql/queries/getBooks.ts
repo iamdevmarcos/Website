c69b58f410b87545c2b0b3f07ebd9d725cb1b76d
Marcos Mendes <marcosdev.me@gmail.com>
2023-10-28 13:37:44 -0300

    Add BookAuthor to GraphQL Query

--- src/graphql/queries/getBooks.ts
+++ src/graphql/queries/getBooks.ts
@@ -2,9 +2,10 @@ import { gql } from '@apollo/client'
 
 export const GET_BOOKS = /* GraphQL */ gql`
   query getBooks {
-    books {
+    books(orderBy: bookTitle_ASC) {
       id
       bookTitle
+      bookAuthor
       bookThumbnail {
         width
         height
