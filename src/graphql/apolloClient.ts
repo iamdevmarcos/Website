dec38f753737f007e2a455ad94ef8e990fc11cc8
Marcos Mendes <marcosdev.me@gmail.com>
2023-10-29 10:13:42 -0300

    Add ssrMode to apolloClient

--- src/graphql/apolloClient.ts
+++ src/graphql/apolloClient.ts
@@ -3,6 +3,7 @@ import { ApolloClient, InMemoryCache } from '@apollo/client'
 export const apolloClient = new ApolloClient({
   uri: process.env.HYGRAPH_API_HOST,
   cache: new InMemoryCache(),
+  ssrMode: typeof window === 'undefined',
   headers: {
     authorization: `Bearer ${process.env.HYGRAPH_API_TOKEN}`
   },
