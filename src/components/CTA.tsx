dd9b2239d6f22007197f81cef07ef4766d6f3f8f
Marcos Mendes <marcosdev.me@gmail.com>
2024-01-09 20:12:09 -0300

    Apply Explosion

--- src/components/CTA.tsx
+++ src/components/CTA.tsx
@@ -1,31 +1,40 @@
 import { SiMinutemailer } from 'react-icons/si'
+import ConfettiExplosion from 'react-confetti-explosion'
 
 import { Flex, Link, Text, Icon } from '@chakra-ui/react'
+import { useState } from 'react'
 
-const CTA = () => (
-  <Flex
-    alignItems="center"
-    justifyContent="center"
-    bg="#DEBDFF"
-    color="#101828"
-    textTransform="uppercase"
-    h="3.8rem"
-  >
-    <Link
-      href="mailto:marcosdev.me@gmail.com"
-      _hover={{ textDecor: 'none', opacity: 0.6 }}
-      display="flex"
+const CTA = () => {
+  const [isExploding, setIsExploding] = useState(false)
+
+  return (
+    <Flex
       alignItems="center"
       justifyContent="center"
-      gap={2}
-      textAlign="center"
-      fontWeight="semibold"
-      fontSize={{ base: '0.8rem', md: '1.2rem' }}
+      bg="#DEBDFF"
+      color="#101828"
+      textTransform="uppercase"
+      h="3.8rem"
     >
-      <Text>let’s bring your project to the real world - contact me</Text>
-      <Icon as={SiMinutemailer} display={{ base: 'none', md: 'block' }} />
-    </Link>
-  </Flex>
-)
+      {isExploding && <ConfettiExplosion />}
+
+      <Link
+        onClick={() => setIsExploding(true)}
+        href="mailto:marcosdev.me@gmail.com"
+        _hover={{ textDecor: 'none', opacity: 0.6 }}
+        display="flex"
+        alignItems="center"
+        justifyContent="center"
+        gap={2}
+        textAlign="center"
+        fontWeight="semibold"
+        fontSize={{ base: '0.8rem', md: '1.2rem' }}
+      >
+        <Text>let’s bring your project to the real world - contact me</Text>
+        <Icon as={SiMinutemailer} display={{ base: 'none', md: 'block' }} />
+      </Link>
+    </Flex>
+  )
+}
 
 export default CTA
