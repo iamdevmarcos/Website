e19e06f9a8db41838fe68a67c06b6d79aeaa7a7a
Marcos Mendes <marcosdev.me@gmail.com>
2024-02-17 18:23:52 -0300

    Update Videos List

--- src/mocks/videos.ts
+++ src/mocks/videos.ts
@@ -1,5 +1,9 @@
 export default [
   {
+    title: 'Easiest Way to Create Type Inference using Zod/Yup',
+    videoId: 'c8fl9SqY0go'
+  },
+  {
     title:
       'Mastering Type-Safe Translations with i18-next in React Native & Expo',
     videoId: 'GV4jyDGhhtc'
