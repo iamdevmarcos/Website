b7096561866197e2b850ff6623920d08de672400
Marcos Mendes <marcosdev.me@gmail.com>
2024-05-02 20:12:23 -0300

    Remove easter egg

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -4,12 +4,9 @@ import { Box } from '@chakra-ui/react'
 
 import Footer from './Footer'
 import Header from './Header'
-import AnimatedElement from './AnimatedElement'
 
 const Layout = ({ children }: PropsWithChildren) => (
   <Box minH="100vh" bg="black">
-    <AnimatedElement />
-
     <Header />
     {children}
     <Footer />
