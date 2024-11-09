fe2cea37fc9e3b85bb82fba8b1514e3bf0fb5c7a
Marcos Mendes <marcosdev.me@gmail.com>
2024-11-09 11:32:57 -0300

    Update header

--- src/components/Logo.tsx
+++ src/components/Logo.tsx
@@ -1,16 +1,20 @@
 import Link from 'next/link'
 
-import { Text } from '@chakra-ui/react'
+import { Avatar, Flex, Text } from '@chakra-ui/react'
 
 const Logo = () => (
   <Link href="/" aria-label="Logo">
-    <Text
-      color="white"
-      fontSize={{ base: '1.4rem', sm: '1.6rem' }}
-      fontWeight="semibold"
-    >
-      Marcos Mendes
-    </Text>
+    <Flex alignItems="center" gap="12px">
+      <Avatar borderRadius="md" src="https://github.com/iamdevmarcos.png" />
+
+      <Text
+        color="white"
+        fontSize={{ base: '1.4rem', sm: '1.6rem' }}
+        fontWeight="semibold"
+      >
+        Marcos Mendes
+      </Text>
+    </Flex>
   </Link>
 )
 
