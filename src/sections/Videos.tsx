7e248cd6febb14940105e4c569b78cdd0c30ab96
Marcos Mendes <marcosdev.me@gmail.com>
2024-11-19 14:53:52 -0300

    style: update styles for videos section

--- src/sections/Videos.tsx
+++ src/sections/Videos.tsx
@@ -1,4 +1,4 @@
-import { AspectRatio, Flex } from '@chakra-ui/react'
+import { AspectRatio, Box, Flex, Text } from '@chakra-ui/react'
 import { Container, Section } from 'components'
 import { YoutubeVideo } from 'types/YoutubeVideo'
 
@@ -7,23 +7,49 @@ const Videos = ({ videos }: { videos: YoutubeVideo[] }) => {
     <Container>
       <Flex
         as="section"
-        aria-label="Tutorials and Live coding"
+        aria-label="Videos and Live coding"
         mt={{ base: '6rem', sm: '8rem' }}
         flexDir="column"
         justifyContent="flex-start"
         gap={10}
       >
-        <Section heading="Tutorials and Live Coding">
-          <Flex flexDir="column" gap={{ base: '2.4rem', md: '6.4rem' }}>
+        <Section heading="Videos and Live Coding">
+          <Flex
+            flexWrap="wrap"
+            gap={{ base: '2.4rem', md: '4rem' }}
+            justifyContent="center"
+          >
             {videos.map(({ title, video_id }) => (
-              <AspectRatio key={video_id} maxW="100%" maxH="45rem" ratio={1}>
-                <iframe
-                  title={title}
-                  src={`https://www.youtube.com/embed/${video_id}`}
-                  allowFullScreen
-                  allow="accelerometer; clipboard-write; encrypted-media; gyroscope; picture-in-picture;"
-                />
-              </AspectRatio>
+              <Box
+                key={video_id}
+                w={{ base: '100%', md: '25rem' }}
+                bg="gray.900"
+                borderRadius="md"
+                overflow="hidden"
+                boxShadow="lg"
+                p="4"
+                border="5px solid #333"
+              >
+                <AspectRatio ratio={16 / 9}>
+                  <iframe
+                    title={title}
+                    src={`https://www.youtube.com/embed/${video_id}`}
+                    allowFullScreen
+                    allow="accelerometer; clipboard-write; encrypted-media; gyroscope; picture-in-picture;"
+                  />
+                </AspectRatio>
+
+                <Box pt="4">
+                  <Text
+                    fontWeight="semibold"
+                    fontSize="1.3rem"
+                    color="white"
+                    letterSpacing="-0.8px"
+                  >
+                    {title}
+                  </Text>
+                </Box>
+              </Box>
             ))}
           </Flex>
         </Section>
